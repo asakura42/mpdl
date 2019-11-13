1bae1e3cfc2ab909f103695e8fffd89b8ea69e97
whew <51290160+whew@users.noreply.github.com>
2019-11-13 00:25:24 +0000

    Update beets.sh

--- beets.sh
+++ beets.sh
@@ -3,5 +3,4 @@ command -v beets >/dev/null 2>&1 || { echo >&2 "I require foo but it's not insta
 command -v chromaprint >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
 command -v python-pyacoustid >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
 command -v python-pylast >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
-echo "Don't forget to properly config your Beets! Demo config.yaml based in repository (https://github.com/asakura42/mpdg)"
 beets import -s -q $1
