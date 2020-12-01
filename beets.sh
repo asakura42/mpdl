2c56744ae2e2d568540f970dd548e4dc2cc2a7aa
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-12-01 01:25:16 +0000

    Update beets.sh

--- beets.sh
+++ beets.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-command -v beets >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
-command -v chromaprint >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
-command -v python-pyacoustid >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
-command -v python-pylast >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; }
+command -v beets >/dev/null 2>&1 || { echo >&2 "sudo pacman -S beets"; }
+command -v chromaprint >/dev/null 2>&1 || { echo >&2 "sudo pacman -S chromaprint"; }
+command -v python-pyacoustid >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pyacoustid"; }
+command -v python-pylast >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pylast"; }
+command -v tageditor >/dev/null 2>&1 || { echo >&2 "yay -S tageditor"; }
 beet import -s -q $1
