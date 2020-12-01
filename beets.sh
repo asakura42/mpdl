2076b782fec14c4d79d585a7d60ec7c65795dd4c
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-12-01 01:27:25 +0000

    Update beets.sh

--- beets.sh
+++ beets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 command -v beet >/dev/null 2>&1 || { echo >&2 "sudo pacman -S beets"; }
-command -v chromaprint >/dev/null 2>&1 || { echo >&2 "sudo pacman -S chromaprint"; }
-command -v python-pyacoustid >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pyacoustid"; }
-command -v python-pylast >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pylast"; }
+pacman -Q chromaprint >/dev/null 2>&1 || { echo >&2 "sudo pacman -S chromaprint"; }
+pacman -Q python-pyacoustid >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pyacoustid"; }
+pacman -Q python-pylast >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pylast"; }
 command -v tageditor >/dev/null 2>&1 || { echo >&2 "yay -S tageditor"; }
 [ -z "$1" ] && beet import -s -q . || beet import -s -q "$1"
