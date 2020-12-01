826329577ef5c6b4887716b375bb88da16282ae7
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-12-01 01:26:30 +0000

    Update beets.sh

--- beets.sh
+++ beets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-command -v beets >/dev/null 2>&1 || { echo >&2 "sudo pacman -S beets"; }
+command -v beet >/dev/null 2>&1 || { echo >&2 "sudo pacman -S beets"; }
 command -v chromaprint >/dev/null 2>&1 || { echo >&2 "sudo pacman -S chromaprint"; }
 command -v python-pyacoustid >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pyacoustid"; }
 command -v python-pylast >/dev/null 2>&1 || { echo >&2 "sudo pacman -S python-pylast"; }
