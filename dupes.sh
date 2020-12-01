5e5aff3108f5d6ff8c31aa436bed233eeb1667ed
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-12-01 01:19:43 +0000

    Delete dupes.sh

--- dupes.sh
+++
@@ -1,8 +0,0 @@
-# Отобрать файлы со скобками
-{ find *[0-9]\).opus & find *[0-9]\).opus | sed -e "s/ ([0-9])//"; } | sort | uniq > /tmp/finddupes.txt
-while read -r line ; do
-	echo "$line $(ffprobe "$line" 2>&1 | grep -oP '(?<=bitrate: )[0-9]+')";
-done < /tmp/finddupes.txt > /tmp/dupes.txt
-cat /tmp/dupes.txt | sort | uniq
-
-
