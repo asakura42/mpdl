c17298ef60f9077e21537ea1ff74f81dbc0c0072
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-12-01 01:42:09 +0000

    Update fixgenres.sh

--- fixgenres.sh
+++ fixgenres.sh
@@ -4,7 +4,7 @@
 
 for f in *.{opus,m4a,mp3}
 do
-	tageditor -g -f $f | grep "Genre.*;"
+	tageditor -g -f "$f" | grep "Genre.*;"
 	result=$?
 	if (( result == 0 )); then
 		tageditor set +genre="$gen1" +genre="$gen2" -f "$f"
