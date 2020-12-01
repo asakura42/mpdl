dcf7b7d6112c88d62ba540101bb953e39e26da19
asakura42 <51290160+asakura42@users.noreply.github.com>
2020-12-01 01:50:46 +0000

    Update fixgenres.sh

--- fixgenres.sh
+++ fixgenres.sh
@@ -7,6 +7,9 @@ do
 	tageditor -g -f "$f" | grep "Genre.*;"
 	result=$?
 	if (( result == 0 )); then
+		genres=$(tageditor -g -f "$f" | grep "Genre.*;" | sed 's/^ *.*Genre *//')
+		gen1=$(awk -F ';' '{ print $1 }' <<< "$genres")
+		gen2=$(awk -F ';' '{ print $2 }' <<< "$genres")
 		tageditor set +genre="$gen1" +genre="$gen2" -f "$f"
 	fi
 done
